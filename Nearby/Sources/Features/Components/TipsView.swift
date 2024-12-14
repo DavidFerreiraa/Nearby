f2dc8ff18ca786912ba44fdbfa5c9f171e755ef1
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-14 17:29:27 -0300

    feat: add TipsView

--- Nearby/Sources/Features/Components/TipsView.swift
+++ Nearby/Sources/Features/Components/TipsView.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+import UIKit
+
+public class TipsView: UIView {
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = Typography.titleSM
+        label.textColor = Colors.gray600
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = Typography.textSM
+        label.textColor = Colors.gray500
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    init(icon: UIImage, title: String, description: String) {
+        super.init(frame: .zero)
+        setupUI()
+        setupComponent(icon: icon, title: title, description: description)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        self.addSubview(iconImageView)
+        self.addSubview(titleLabel)
+        self.addSubview(descriptionLabel)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            iconImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            iconImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            iconImageView.heightAnchor.constraint(equalToConstant: 24),
+            iconImageView.widthAnchor.constraint(equalToConstant: 24),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    private func setupComponent(icon: UIImage,
+                                title: String,
+                                description: String)
+    {
+        iconImageView.image = icon
+        titleLabel.text = title
+        descriptionLabel.text = description
+    }
+}
