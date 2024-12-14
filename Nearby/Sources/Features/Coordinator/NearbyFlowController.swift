5b992f4b6f42c4116695f4427ad88bb4fc5496a1
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-12-14 17:30:10 -0300

    feat: add SplashFlowDelegate to NearbyFlowController

--- Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
+++ Nearby/Sources/Features/Coordinator/NearbyFlowController.swift
@@ -9,6 +9,7 @@ import Foundation
 import UIKit
 
 class NearbyFlowController {
+    
     private var navigationController: UINavigationController?
     
     public init() {
@@ -18,9 +19,17 @@ class NearbyFlowController {
     func start() -> UINavigationController? {
         let contentView = SplashView()
         
-        let startViewController = SplashViewController(contentView: contentView)
+        let startViewController = SplashViewController(contentView: contentView, delegate: self)
         self.navigationController = UINavigationController(rootViewController: startViewController)
         
         return navigationController
+    }
+}
+
+extension NearbyFlowController: SplashFlowDelegate {
+    func decideNavigationFlow() {
+        let contentView = WelcomeView()
+        let welcomeViewcontroller = WelcomeViewController(contentView: contentView)
+        navigationController?.pushViewController(welcomeViewcontroller, animated: true)
     }
 }
